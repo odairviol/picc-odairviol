1b32804b0cb99add92079c2b52e9c379511a4b7d
odairviol <odair.viol@gmail.com>
2014-09-01 20:34:37 -0300

    Primeiro aula treinamento android.

---
+++ 01-09-2014/Treinamento/src/br/com/cast/treinamento/MeuOnclickListener.java
@@ -0,0 +1,14 @@
+package br.com.cast.treinamento;
+
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MeuOnclickListener implements OnClickListener {
+
+	@Override
+	public void onClick(View arg0) {
+		
+	}
+
+}
